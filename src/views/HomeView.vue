7880649b984a2d893e3b464d843303396bfcc77f
elvinkyungu <elvinkyungu.75@gmail.com>
2024-09-22 13:58:51 +0200

    feat: Add progress indicator for image display duration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,13 @@ import { ref, onMounted } from 'vue'
 import { gsap } from 'gsap'
 import image1 from '@/assets/tokyo-1.avif'
 import image2 from '@/assets/tokyo-2.avif'
-import image3 from '@/assets/tokyo-1.avif'
 
-const images = ref([image1, image2, image3])
+const images = ref([image1, image2])
 const currentIndex = ref(0)
 const slideContainer = ref<HTMLElement | null>(null)
+const progressBar = ref<HTMLElement | null>(null)
+
+const duration = 2000 // Durée d'affichage de chaque image en ms
 
 const animateSlides = () => {
   gsap.to(slideContainer.value, {
@@ -17,14 +19,29 @@ const animateSlides = () => {
     ease: 'power1.inOut',
     onComplete: () => {
       currentIndex.value = (currentIndex.value + 1) % images.value.length
-      setTimeout(animateSlides, 1000) 
+      resetProgressBar()
+      setTimeout(animateSlides, 2000) 
     }
   })
 }
 
+const resetProgressBar = () => {
+  if (progressBar.value) {
+    gsap.set(progressBar.value, { width: '0%' })
+    gsap.to(progressBar.value, {
+      width: '100%',
+      duration: duration / 1000,
+      ease: 'linear'
+    })
+  }
+}
+
 onMounted(() => {
-  slideContainer.value.style.width = `${images.value.length * 100}%`
-  setTimeout(animateSlides, 1000) 
+  if (slideContainer.value) {
+    slideContainer.value.style.width = `${images.value.length * 100}%`
+  }
+  resetProgressBar()
+  setTimeout(animateSlides, 2000) 
 })
 </script>
 
@@ -32,20 +49,30 @@ onMounted(() => {
   <Header />
   <main class="relative w-full h-[90vh] flex overflow-hidden">
     <div ref="slideContainer" class="flex h-full">
+      
       <img
         v-for="(image, index) in images"
         :key="index"
         :src="image"
         alt="Image"
         class="object-cover"
-        style="flex:  0 0 100%"
+        style="flex: 0 0 100%"
       />
     </div>
+    <div ref="progressBar" class="absolute bottom-0 left-0 h-1 bg-green-500" />
   </main>
 </template>
 
 <style scoped>
 .flex {
   display: flex;
+}
+.progress-bar {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 5px; /* Hauteur de l'indicateur */
+  background-color: green; /* Couleur de l'indicateur */
+  transition: width 0.5s; /* Transition pour un remplissage fluide */
 }
 </style>
