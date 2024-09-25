b8b3b17c133380b456d1156ab024320ab074c5ad
elvinkyungu <elvinkyungu.75@gmail.com>
2024-09-25 13:50:03 +0200

    fix text issues and responsive side

--- src/components/ScrollTriggerSection.vue
+++ src/components/ScrollTriggerSection.vue
@@ -60,25 +60,25 @@ const slides = [
         class="w-full h-full gap-5 relative text-white py-10 md:py-20 lg:py-32 grid grid-cols-12 px-5 md:px-10 lg:px-28 image"
         :class="slide.color"
       >
-        <div class="col-span-3 relative w-full">
+        <div class="md:col-span-3 col-span-6 mt-10 md:mt-0 relative w-full">
           <div class="flex w-full gap-3 justify-center items-center">
             <p class=" ">01</p>
             <p class="h-[1px] bg-white w-full"></p>
             <p class="">06</p>
           </div>
         </div>
-        <div class="col-span-8 relative">
-          <img 
+        <div class="col-span-12 md:col-span-8 relative">
+          <img
             src="https://plus.unsplash.com/premium_photo-1664202526559-e21e9c0fb46a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt=""
-            class="absolute w-full h-[42rem] object-cover rounded-lg"
+            class="absolute w-full md:h-[30rem] h-[20rem] lg:h-[42rem] object-cover rounded-lg"
           >
         </div>
         <div class="col-span-1"></div>
-        <div class="absolute bottom-1/4 left-[21%] ">
-          <div class="-space-y-10">
-            <h1 class="text-[6rem] font-medium">Welcome to tokyo</h1>
-            <h1 class="text-[6rem] font-medium text-strok">Live the beauty</h1>
+        <div class="absolute bottom-[65%] left-[3%] lg:bottom-64 lg:left-[21%] ">
+          <div class="md:-space-y-1 lg:-space-y-1">
+            <h1 class="text-3xl md:text-6xl lg:text-[6rem] font-medium">Welcome to tokyo</h1>
+            <h1 class="text-3xl md:text-6xl lg:text-[6rem] font-medium text-strok">Live the beauty</h1>
           </div>
           <p class="text-lg md:text-xl lg:text-2xl top-5">Vivez des moment uniques</p>
         </div>
