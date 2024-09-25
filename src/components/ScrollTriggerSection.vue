0ee1f4d15a6acdbac014f00dfbcb6a1518f0deae
elvinkyungu <elvinkyungu.75@gmail.com>
2024-09-25 13:34:50 +0200

    fix images and same animation in ScrollTrigger component

--- src/components/ScrollTriggerSection.vue
+++ src/components/ScrollTriggerSection.vue
@@ -42,7 +42,7 @@ onUnmounted(() => {
 })
 
 const slides = [
-  { id: 'one', color: 'bg-teal-700' },
+  { id: 'one', color: 'bg-black' },
   { id: 'two', color: 'bg-orange-700' },
   { id: 'three', color: 'bg-emerald-700' },
   { id: 'four', color: 'bg-indigo-700' },
@@ -57,12 +57,39 @@ const slides = [
       <div
         v-for="slide in slides"
         :key="slide.id"
-        class="w-full h-full text-[60px] text-white flex justify-center items-center image"
+        class="w-full h-full gap-5 relative text-white py-10 md:py-20 lg:py-32 grid grid-cols-12 px-5 md:px-10 lg:px-28 image"
         :class="slide.color"
       >
-        {{ slide.id }}
+        <div class="col-span-3 relative w-full">
+          <div class="flex w-full gap-3 justify-center items-center">
+            <p class=" ">01</p>
+            <p class="h-[1px] bg-white w-full"></p>
+            <p class="">06</p>
+          </div>
+        </div>
+        <div class="col-span-8 relative">
+          <img 
+            src="https://plus.unsplash.com/premium_photo-1664202526559-e21e9c0fb46a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt=""
+            class="absolute w-full h-[42rem] object-cover rounded-lg"
+          >
+        </div>
+        <div class="col-span-1"></div>
+        <div class="absolute bottom-1/4 left-[21%] ">
+          <div class="-space-y-10">
+            <h1 class="text-[6rem] font-medium">Welcome to tokyo</h1>
+            <h1 class="text-[6rem] font-medium text-strok">Live the beauty</h1>
+          </div>
+          <p class="text-lg md:text-xl lg:text-2xl top-5">Vivez des moment uniques</p>
+        </div>
       </div>
     </div>
-    <div class="w-screen h-screen bg-violet-200"></div>
   </div>
 </template>
+<style>
+.text-strok {
+  color: transparent;
+  text-shadow: none;
+  -webkit-text-stroke: 2px rgba(255, 255, 255, 0.9);
+}
+</style>
