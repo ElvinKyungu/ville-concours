92792c94daa2fe98501d113e01372016382811a6
elvinkyungu <elvinkyungu.75@gmail.com>
2024-09-25 08:27:50 +0200

    add images slide component animation logic with GSAP

--- src/components/HomeCardSlider.vue
+++ src/components/HomeCardSlider.vue
@@ -11,37 +11,35 @@ const images = [img1, img2, img3]
 
 const leftImage = ref(images[images.length - 1])
 const centerImage = ref(images[0])
-const rightImage = ref(images[1]) 
-const currentIndex = ref(0)  
+const rightImage = ref(images[1])
+const currentIndex = ref(0)
 
 onMounted(() => {
-  gsap.set(".left-image", { x: "-100%", position: "absolute" })
-  gsap.set(".center-image", { x: "0%", position: "absolute" })
-  gsap.set(".right-image", { x: "100%", position: "absolute" })
+  resetImageOpacity()
 })
 
 const slideRight = () => {
-  gsap.to(".center-image", { x: "-100%", duration: 0.5 }) 
-  gsap.to(".right-image", { x: "0%", duration: 0.5 })
-  gsap.to(".left-image", { x: "100%", duration: 0 })
-
-  gsap.delayedCall(0.5, () => {
+  fadeOutAll(() => {
     currentIndex.value = (currentIndex.value + 1) % images.length
     updateImages()
-    resetImagePositions()
+    fadeInAll()
   })
 }
 
 const slideLeft = () => {
-  gsap.to(".center-image", { x: "100%", duration: 0.5 }) 
-  gsap.to(".left-image", { x: "0%", duration: 0.5 })
-  gsap.to(".right-image", { x: "-100%", duration: 0 })
-
-  gsap.delayedCall(0.5, () => {
+  fadeOutAll(() => {
     currentIndex.value = (currentIndex.value - 1 + images.length) % images.length
     updateImages()
-    resetImagePositions()
+    fadeInAll()
   })
+}
+
+const fadeOutAll = (callback: () => void) => {
+  gsap.to(".left-image, .center-image, .right-image", { opacity: 0, duration: 0.2, onComplete: callback })
+}
+
+const fadeInAll = () => {
+  gsap.to(".left-image, .center-image, .right-image", { opacity: 1, duration: 0.2 })
 }
 
 const updateImages = () => {
@@ -50,31 +48,17 @@ const updateImages = () => {
   rightImage.value = images[(currentIndex.value + 1) % images.length]
 }
 
-const resetImagePositions = () => {
-  gsap.set(".left-image", { x: "-100%" })
-  gsap.set(".center-image", { x: "0%" })
-  gsap.set(".right-image", { x: "100%" })
+const resetImageOpacity = () => {
+  gsap.set(".left-image, .center-image, .right-image", { opacity: 1 })
 }
 </script>
 
 <template>
-  <section class="grid grid-cols-12 gap-4">
-    <div class="col-span-3 relative overflow-hidden h-64">
-      <div class="absolute inset-0 flex justify-center items-center">
-        <img class="left-image w-full h-full object-cover" :src="leftImage" alt="Left Image">
-      </div>
+  <section class="grid grid-cols-12 gap-4 relative mb-5">
+    <div class="col-span-3 overflow-hidden">
+      <p class="text-center text-xs font-bold">01/02</p>
     </div>
-
-    <div class="col-span-6 relative overflow-hidden h-64">
-      <div class="absolute inset-0 flex justify-center items-center">
-        <img class="center-image w-full h-full object-cover" :src="centerImage" alt="Center Image">
-      </div>
-    </div>
-
-    <div class="col-span-3 relative overflow-hidden h-64">
-      <div class="absolute inset-0 flex justify-center items-center">
-        <img class="right-image w-full h-full object-cover" :src="rightImage" alt="Right Image">
-      </div>
+    <div class="col-span-6 overflow-hidden ">
       <div class="flex justify-center gap-4 mt-4">
         <p class="px-5 bg-black rounded-full py-1 cursor-pointer" @click="slideLeft">
           <IconLeft class="text-white"/>
@@ -84,11 +68,33 @@ const resetImagePositions = () => {
         </p>
       </div>
     </div>
+    <div class="col-span-3 overflow-hidden">
+      <p class="text-center text-xs font-bold">03/03</p>
+    </div>
+  </section>
+  <section class="grid grid-cols-12 gap-4 relative">
+    <div class="col-span-3 overflow-hidden h-72">
+      <div class="relative flex justify-center items-center h-full w-full">
+        <img class="left-image w-full h-full object-cover absolute" :src="leftImage" alt="Left Image">
+      </div>
+    </div>
+
+    <div class="col-span-6 overflow-hidden h-96">
+      <div class="relative flex justify-center items-center h-full w-full">
+        <img class="center-image w-full h-full object-cover absolute" :src="centerImage" alt="Center Image">
+      </div>
+    </div>
+
+    <div class="col-span-3 overflow-hidden h-72">
+      <div class="relative flex justify-center items-center h-full w-full">
+        <img class="right-image w-full h-full object-cover absolute" :src="rightImage" alt="Right Image">
+      </div>
+    </div>
   </section>
 </template>
 
 <style scoped>
-.left-image, .center-image, .right-image {
-  position: absolute;
-}
+  .left-image, .center-image, .right-image {
+    transition: opacity 0.5s ease;
+  }
 </style>
