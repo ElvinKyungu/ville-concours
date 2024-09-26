78d06d4727b1454e5f7ff651d8416a3c436dbe02
elvinkyungu <elvinkyungu.75@gmail.com>
2024-09-26 21:36:34 +0200

    fix footer on responsive and same colors

--- src/components/base/Footer.vue
+++ src/components/base/Footer.vue
@@ -4,11 +4,11 @@
     <div class="sm:flex sm:items-center sm:justify-between">
       <router-link 
         to="/" 
-        class="flex items-center rtl:space-x-reverse text-3xl md:text-5xl"
+        class="flex items-center mb-5 md:mb-0 rtl:space-x-reverse text-3xl md:text-5xl"
       >
         Tokyo city
       </router-link>
-      <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+      <ul class="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 ">
         <li>
           <router-link  to="/" class="hover:underline me-4 md:me-6">About</router-link>
         </li>
@@ -24,7 +24,7 @@
       </ul>
     </div>
     <hr class="my-6 border-gray-300 sm:mx-auto lg:my-8" />
-    <span class="block text-sm text-gray-700 sm:text-center">
+    <span class="block text-sm sm:text-center">
       © 2024
       <router-link  to="/" class="hover:underline">
         Tokyo city
