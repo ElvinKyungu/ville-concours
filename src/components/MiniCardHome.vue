156d5d98f7e94c7752fc681f34ffc5b0380fc8d8
elvinkyungu <elvinkyungu.75@gmail.com>
2024-09-26 22:02:32 +0200

    add Minicard home component

---
+++ src/components/MiniCardHome.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import img1 from '@/assets/trigger-1.avif'
+import img2 from '@/assets/trigger-2.avif'
+import img3 from '@/assets/trigger-3.avif'
+import img4 from '@/assets/trigger-4.avif'
+import img5 from '@/assets/trigger-5.avif'
+import img6 from '@/assets/trigger-6.avif'
+const cards = [
+  {
+    title: "Shibuya Crossing",
+    description: "The iconic intersection of Tokyo’s energy.",
+    image: img1,
+  },
+  {
+    title: "Tokyo Skytree",
+    description: "Stunning views from the city's tallest tower.",
+    image: img2,
+  },
+  {
+    title: "Senso-ji Temple",
+    description: "Tokyo's oldest temple with timeless charm.",
+    image: img3,
+  },
+  {
+    title: "Meiji Shrine",
+    description: "A peaceful escape in the heart of Tokyo.",
+    image: img4,
+  },
+  {
+    title: "Akihabara",
+    description: "The ultimate hub for tech and anime lovers.",
+    image: img5,
+  },
+  {
+    title: "Tsukiji Market",
+    description: "Fresh seafood and a taste of Tokyo’s cuisine.",
+    image: img6,
+  }
+]
+</script>
+
+<template>
+  <section class="flex flex-col w-full px-5 md:px-20 py-10 md:py-20 lg:py-28">
+    <p>Welcome</p>
+    <h1 class="text-heading-1 text-black">Discover the Top 6 Must-See Spots in Tokyo</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-4 md:gap-y-10 mt-10">
+      <div 
+        v-for="(card, index) in cards" 
+        :key="index" 
+        class="p-7 bg-gray hover:bg-black/10 transition rounded-lg group duration-700 cursor-pointer"
+      >
+        <div class="flex gap-5">
+          <div class="w-14 h-14 rounded-lg bg-white relative group-hover:bg-black/10 transition-colors duration-700">
+            <img :src="card.image" alt="" class="img-card-home">
+          </div>
+          <div>
+            <h4 class="text-xl ">{{ card.title }}</h4>
+            <p class="">{{ card.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,96 +12,28 @@ import HomeCardSlider from '@/components/HomeCardSlider.vue'
 import PlacesCardClickable from '@/components/PlacesCardClickable.vue'
 import ScrollTriggerSection from '@/components/ScrollTriggerSection.vue'
 import FAQ from '@/components/FAQ.vue'
+import MiniCardHome from '@/components/MiniCardHome.vue'
 
 const imagesList = [image1,image3,image4,]
 </script>
 
 <template>
   <!-- <Header/> -->
-  <section class="px-5 md:px-20 py-10 md:py-20 lg:py-28">
-    <div class="grid grid-cols-12">
-      <div class="col-span-12 md:col-span-4 my-5 md:my-10 mt-2">
+  <section class="px-5 md:px-20 py-10 md:pt-20 lg:pt-20">
+    <div class="grid grid-cols-12 gap-4">
+      <div class="col-span-12 md:col-span-3 my-5 mt-2">
         <p class="h-[1px] bg-black"></p>
-        <p class="text-sm">Nous sommes</p>
+        <p class="text-sm">W'are Tokyo</p>
       </div>
-      <h1 class="col-span-12 md:col-span-8 text-3xl md:text-5xl lg:text-5xl">
-        La belle ville touristique au monde 
+      <h1 class="col-span-12 md:col-span-9 text-3xl md:text-5xl lg:text-5xl">
+        Welcome to Tokyo, the most vibrant city in the world
       </h1>
     </div>
-    <h1 class="text-3xl md:text-5xl lg:text-5xl">Innovation Labs accelerates the future of home improvement retail－through the lens of emerging technology. </h1>
+    <h1 class="text-3xl md:text-5xl lg:text-5xl">
+      Tokyo sets the pace for the future of urban living—through the lens of culture, cutting-edge technology, and endless exploration.
+    </h1>
   </section>
-  <section class="flex flex-col w-full px-5 md:px-20 py-10 md:py-20 lg:py-28 ">
-    <p>Something</p>
-    <h1 class="text-heading-1 text-black">Title text</h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-4 md:gap-y-10 mt-10">
-      <div class="p-7 bg-gray hover:bg-black transition rounded-lg group duration-700 cursor-pointer">
-        <div class="flex gap-5">
-          <div class="w-14 h-14 rounded-lg bg-white relative group-hover:bg-black/10 transition-colors duration-700">
-            <img src="@/assets/tokyo-1.avif" alt="" class="img-card-home">
-          </div>
-          <div>
-            <h4 class="text-2xl group-hover:text-white font-medium transition duration-700">Same text</h4>
-            <p class="group-hover:text-white transition duration-700">Same paragraphs</p>
-          </div>
-        </div>
-      </div>
-      <div class="p-7 bg-gray hover:bg-black transition rounded-lg group duration-700 cursor-pointer">
-        <div class="flex gap-5">
-          <div class="w-14 h-14 rounded-lg bg-white relative group-hover:bg-black/10 transition-colors duration-700">
-            <img src="@/assets/tokyo-1.avif" alt="" class="img-card-home">
-          </div>
-          <div>
-            <h4 class="text-2xl group-hover:text-white font-medium transition duration-700">Same text</h4>
-            <p class="group-hover:text-white transition duration-700">Same paragraphs</p>
-          </div>
-        </div>
-      </div>
-      <div class="p-7 bg-gray hover:bg-black transition rounded-lg group duration-700 cursor-pointer">
-        <div class="flex gap-5">
-          <div class="w-14 h-14 rounded-lg bg-white relative group-hover:bg-black/10 transition-colors duration-700">
-            <img src="@/assets/tokyo-1.avif" alt="" class="img-card-home">
-          </div>
-          <div>
-            <h4 class="text-2xl group-hover:text-white font-medium transition duration-700">Same text</h4>
-            <p class="group-hover:text-white transition duration-700">Same paragraphs</p>
-          </div>
-        </div>
-      </div>
-      <div class="p-7 bg-gray hover:bg-black transition rounded-lg group duration-700 cursor-pointer">
-        <div class="flex gap-5">
-          <div class="w-14 h-14 rounded-lg bg-white relative group-hover:bg-black/10 transition-colors duration-700">
-            <img src="@/assets/tokyo-1.avif" alt="" class="img-card-home">
-          </div>
-          <div>
-            <h4 class="text-2xl group-hover:text-white font-medium transition duration-700">Same text</h4>
-            <p class="group-hover:text-white transition duration-700">Same paragraphs</p>
-          </div>
-        </div>
-      </div>
-      <div class="p-7 bg-gray hover:bg-black transition rounded-lg group duration-700 cursor-pointer">
-        <div class="flex gap-5">
-          <div class="w-14 h-14 rounded-lg bg-white relative group-hover:bg-black/10 transition-colors duration-700">
-            <img src="@/assets/tokyo-1.avif" alt="" class="img-card-home">
-          </div>
-          <div>
-            <h4 class="text-2xl group-hover:text-white font-medium transition duration-700">Same text</h4>
-            <p class="group-hover:text-white transition duration-700">Same paragraphs</p>
-          </div>
-        </div>
-      </div>
-      <div class="p-7 bg-gray hover:bg-black transition rounded-lg group duration-700 cursor-pointer">
-        <div class="flex gap-5">
-          <div class="w-14 h-14 rounded-lg bg-white relative group-hover:bg-black/10 transition-colors duration-700">
-            <img src="@/assets/tokyo-1.avif" alt="" class="img-card-home">
-          </div>
-          <div>
-            <h4 class="text-2xl group-hover:text-white font-medium transition duration-700">Same text</h4>
-            <p class="group-hover:text-white transition duration-700">Same paragraphs</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <MiniCardHome/>
   <SectionHighlight/>
   <main class="relative py-10">
     <div
